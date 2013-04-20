4273d0185d6ad774ae2cebafbbacffd9d3747ef1
Shah Mohsin WAHED <shahmohsin.wahed@gmail.com>
2013-04-20 20:17:51 +0200

    modified:   CHANGES.txt fixed version number 	modified:   netapp.config.maker.py fixed version number

--- netappConfigMaker/netapp.config.maker.py
+++ netappConfigMaker/netapp.config.maker.py
@@ -8,7 +8,7 @@ Script generate custom netapp filer .config and .rc based on XML config file
 __author__ = "Shah Mohsin WAHED <s.wahed@laposte.net>"
 __copyright__ = "Copyright (c) 2013 S.WAHED"
 __license__ = "GPL"
-__version__ = "1.1"
+__version__ = "1.1.0"
 __cvsversion__ = "$Revision: $"
 __date__ = "$Date: $"
 
