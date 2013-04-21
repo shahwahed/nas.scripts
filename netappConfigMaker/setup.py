523992be3d821bb6dc134ef3f8a0743b41f5eb21
Shah Mohsin WAHED <shahmohsin.wahed@gmail.com>
2013-04-21 11:32:22 +0200

    deleted:    netappConfigMaker/__init__.py no package just script so no need 	deleted:    netappConfigMaker/tests/__init__.py no package just script so no need 	modified:   setup.py fixed setup.py

--- netappConfigMaker/setup.py
+++ netappConfigMaker/setup.py
@@ -1,18 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='netappConfigMaker',
     version='1.1.0',
     author='Shah Mohsin WAHED',
     author_email='shahmohsin.wahed@gmail.com',
-    packages=['netappConfigMaker', 'netappConfigMaker.test'],
+    #packages=['netappConfigMaker', 'netappConfigMaker.test'],
+    scripts=['netapp.config.maker.py', ],
     url='http://pypi.python.org/pypi/netappConfigMaker/',
     license='LICENSE.txt',
     description='Netapp 7Mode Configuration generator based on a XML configuration file.',
     long_description=open('README.txt').read(),
     install_requires=[
-        "argparse => 1.2.1",
-        "lxml => 2.3.5",
+        "argparse >= 1.2.1",
+        "lxml >= 2.3.5",
     ],
 )
-
